6d35f9c3b0096bd57dba71a479aca659a4f0b202
MasterDrak <MasterDrakCoop@proton.me>
2026-02-19 16:53:27 +0000

    Update MainActivity.java

--- android/app/src/main/java/io/github/pdftoimagen/MainActivity.java
+++ android/app/src/main/java/io/github/pdftoimagen/MainActivity.java
@@ -1,7 +1,11 @@
 package io.github.pdftoimagen;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -12,6 +16,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private static final String URL = "https://masterdrak.github.io/pdf-to-image/";
+    private static final int FILE_CHOOSER_REQUEST = 1001;
+
+    private ValueCallback<Uri[]> fileChooserCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +35,30 @@ public class MainActivity extends AppCompatActivity {
         settings.setUseWideViewPort(true);
         settings.setBuiltInZoomControls(true);
         settings.setDisplayZoomControls(false);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
 
         webView.setWebViewClient(new WebViewClient());
+
         webView.setWebChromeClient(new WebChromeClient() {
             @Override
             public void onProgressChanged(WebView view, int p) {
                 progress.setVisibility(p < 100 ? View.VISIBLE : View.GONE);
                 progress.setProgress(p);
+            }
+
+            @Override
+            public boolean onShowFileChooser(WebView view,
+                    ValueCallback<Uri[]> filePathCallback,
+                    FileChooserParams fileChooserParams) {
+                if (fileChooserCallback != null) {
+                    fileChooserCallback.onReceiveValue(null);
+                }
+                fileChooserCallback = filePathCallback;
+
+                Intent intent = fileChooserParams.createIntent();
+                startActivityForResult(intent, FILE_CHOOSER_REQUEST);
+                return true;
             }
         });
 
@@ -42,9 +66,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == FILE_CHOOSER_REQUEST) {
+            Uri[] result = resultCode == Activity.RESULT_OK
+                    ? WebChromeClient.FileChooserParams.parseResult(resultCode, data)
+                    : null;
+            fileChooserCallback.onReceiveValue(result);
+            fileChooserCallback = null;
+        }
+    }
+
+    @Override
     public void onBackPressed() {
         WebView webView = findViewById(R.id.webview);
         if (webView.canGoBack()) webView.goBack();
-        else finish();
+        else super.onBackPressed();
     }
 }
